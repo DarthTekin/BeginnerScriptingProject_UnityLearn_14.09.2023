9fcfcc26e12221f2c216829abd2f18d6b1ded02a
DarthTekin <tekinteks@gmail.com>
2023-09-28 18:43:31 +0300

    Vector3.Lerp
    Vector3.Lerp updated

--- Assets/TestScenes&Scripts/Linear Interpolation/Scripts/Vector3Lerp.cs
+++ Assets/TestScenes&Scripts/Linear Interpolation/Scripts/Vector3Lerp.cs
@@ -7,22 +7,23 @@ public class Vector3Lerp : MonoBehaviour
     Vector3 startVector = Vector3.zero;
     Vector3 endVector = new Vector3(10f, 10f, 10f);
     float turnspeed = 25f;
+    Renderer renderer;
+    MeshRenderer meshRenderer;
     Material material;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = Vector3.Lerp(startVector, endVector, .5f);
-        material = GetComponent<Material>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        //material = GetComponent<Material>();
+        meshRenderer.material.color = Color.Lerp(Color.blue, Color.black, 0.5f * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * -turnspeed * Time.deltaTime);
-        if (material != null )
-        {
-            material.color = Color.Lerp(material.color, Color.black, 0.5f * Time.deltaTime);
-        }
+        //meshRenderer.material.color = Color.Lerp(Color.blue, Color.black, 0.5f * Time.deltaTime);
     }
 }
