be32fbe9ee9a687c8c654e0714a9d9d0641e8698
DarthTekin <tekinteks@gmail.com>
2023-10-02 15:30:43 +0300

    OnMouseDown
    OnMouseDown completed

---
+++ Assets/TestScenes&Scripts/OnMouseDown/Scriots/MouseCheck.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCheck : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked On the box");   
+    }
+}
